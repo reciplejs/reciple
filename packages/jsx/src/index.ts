31385d1147563caf1c78c12eea440490d7f749c2
Cat++ <69035887+catplvsplus@users.noreply.github.com>
2025-11-07 22:21:43 +0800

    no interactives

--- packages/jsx/src/index.ts
+++ packages/jsx/src/index.ts
@@ -1,6 +1,5 @@
 export * from './structures/JSX.js';
 
-export * from './components/interactive/index.js';
 export * from './components/embed/index.js';
 export * from './components/modal/index.js';
 export * from './components/poll/index.js';
