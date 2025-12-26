bf41e08a4d342c104bb029ae869df3f67655ebfa
Cat++ <69035887+catplvsplus@users.noreply.github.com>
2025-12-26 17:29:22 +0800

    custom format for formatting cache entry data

--- packages/registry-cache/src/classes/RegistryCache.ts
+++ packages/registry-cache/src/classes/RegistryCache.ts
@@ -4,6 +4,7 @@ import path from 'node:path';
 import { BaseModule, CommandType, type AnyCommandData, type Config, type MessageCommand } from 'reciple';
 
 export class RegistryCache extends BaseModule implements RegistryCache.Options {
+    public createCacheEntryEvenIfDisabled: boolean = true;
     public cacheDir: string = path.join(process.cwd(), '.cache/reciple-registry/');
     public maxCacheAgeMs?: number;
     public cacheEnabledEnv: string = 'RECIPLE_REGISTRY_CACHE';
@@ -11,12 +12,31 @@ export class RegistryCache extends BaseModule implements RegistryCache.Options {
 
     private readonly logger = useLogger().clone({ label: 'RegistryCache' });
 
+    constructor(options?: RegistryCache.Options) {
+        super();
+        Object.assign(this, options ?? {});
+    }
+
     get cachePath(): string {
         return path.join(this.cacheDir, this.client.application?.id ?? 'cache.json');
     }
 
     public async onReady(): Promise<void> {
-        if (!this.isEnabled()) return;
+        const enabled = this.isEnabled();
+
+        if (enabled || this.createCacheEntryEvenIfDisabled) {
+            this.client.commands?.once('applicationCommandsRegister', async (commands, guildId) => {
+                await this.writeCacheEntry();
+            });
+        }
+
+        if (!enabled) {
+            this.client.modules.on('readyModules', async () => {
+                this.logger.warn(`Registry cache is disabled. Cache is not used.`);
+            });
+
+            return;
+        }
 
         this.logger.debug('Looking for a application commands cache entry...');
         const cachedEntry = await this.readCacheEntry();
@@ -37,8 +57,6 @@ export class RegistryCache extends BaseModule implements RegistryCache.Options {
         this.client.modules.on('readyModules', async () => {
             this.logger.warn(`Application commands are ${this.cached ? '' : 'not '}cached. ${this.cached ? 'Skipping' : 'Proceeding with'} registration.`);
         });
-
-        await this.writeCacheEntry();
     }
 
     public async readCacheEntry(): Promise<RegistryCache.CacheEntry|null> {
@@ -51,8 +69,11 @@ export class RegistryCache extends BaseModule implements RegistryCache.Options {
     public async writeCacheEntry(entry?: RegistryCache.CacheEntry): Promise<void> {
         entry ??= await this.createCacheEntry();
 
+        this.logger.debug('Writing application commands cache entry...');
         await mkdir(this.cacheDir, { recursive: true });
         await writeFile(this.cachePath, JSON.stringify(entry, null, 4), 'utf-8');
+
+        this.logger.debug(`Application commands cache entry written to "${this.cachePath}".`);
     }
 
     public async clearCacheEntry(): Promise<void> {
@@ -60,6 +81,7 @@ export class RegistryCache extends BaseModule implements RegistryCache.Options {
     }
 
     public async createCacheEntry(): Promise<RegistryCache.CacheEntry> {
+        this.logger.debug('Creating current application commands cache entry...');
         const commands = Array.from(this.client.commands?.cache.filter(cmd => cmd.type !== CommandType.Message).map(cmd => cmd.toJSON()).values() ?? []);
 
         for (const command of commands) {
@@ -68,6 +90,10 @@ export class RegistryCache extends BaseModule implements RegistryCache.Options {
 
         const commandsHash = RegistryCache.createCommandsHash(commands);
         const configHash = RegistryCache.createConfigHash(this.client.config?.applicationCommandsRegister ?? {});
+
+        this.logger.debug(`Commands hash: ${commandsHash}`);
+        this.logger.debug(`Config hash: ${configHash}`);
+        this.logger.debug('Current application commands cache entry created.');
 
         return {
             createdAt: Date.now(),
@@ -111,6 +137,10 @@ export class RegistryCache extends BaseModule implements RegistryCache.Options {
 export namespace RegistryCache {
     export interface Options {
         /**
+         * @default true
+         */
+        createCacheEntryEvenIfDisabled?: boolean;
+        /**
          * @default '.cache/reciple-registry/'
          */
         cacheDir?: string;
@@ -130,6 +160,8 @@ export namespace RegistryCache {
         createdAt: number;
     }
 
+    export type CommandData = Exclude<AnyCommandData, MessageCommand.Data>;
+
     export function checkClientIfEnabled(config: Config): boolean {
         return config.applicationCommandsRegister?.registerGlobally
             || config.applicationCommandsRegister?.registerToGuilds === true
@@ -142,7 +174,7 @@ export namespace RegistryCache {
     export function createCommandsHash(commands: Exclude<AnyCommandData, MessageCommand.Data>[]): string {
         const hash = createHash('sha256');
 
-        hash.update(JSON.stringify(commands.map(cmd => cmd)));
+        hash.update(stringifyValue(commands));
 
         return hash.digest('hex');
     }
@@ -151,8 +183,55 @@ export namespace RegistryCache {
         const hash = createHash('sha256');
         const relevantConfig = { applicationCommandsRegister: config.applicationCommandsRegister };
 
-        hash.update(JSON.stringify(relevantConfig));
+        hash.update(stringifyValue(relevantConfig));
 
         return hash.digest('hex');
+    }
+
+    export function stringifyValue(object: any): string {
+        switch (typeof object) {
+            case 'string':
+                return `"${object}"`;
+            case 'number':
+            case 'bigint':
+            case 'boolean':
+            case 'symbol':
+            case 'function':
+                return `"${object.toString()}"`;
+            case 'undefined':
+                return 'undefined';
+            case 'object':
+                if (object === null) {
+                    return 'null';
+                }
+
+                if (Array.isArray(object)) {
+                    return `[${object.map(s => stringifyValue(s)).join(', ')}]`;
+                }
+
+                if (object instanceof Map) {
+                    return `{${Array.from(object.entries()).map(([k, v]) => `${stringifyValue(k)}: ${stringifyValue(v)}`).join(', ')}}`;
+                }
+
+                if (object instanceof Set) {
+                    return `{${Array.from(object.values()).map(v => stringifyValue(v)).join(', ')}}`;
+                }
+
+                if (object instanceof Date) {
+                    return `"${object.toISOString()}"`;
+                }
+
+                try {
+                    const newObject: Record<string, string> = {};
+
+                    for (const [key, value] of Object.entries(object)) {
+                        newObject[key] = stringifyValue(value);
+                    }
+
+                    return JSON.stringify(newObject);
+                } catch {
+                    return String(object);
+                }
+        }
     }
 }
