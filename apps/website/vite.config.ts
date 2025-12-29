400a2d62c35bdb5dce45f8353a76b7ba0881b586
Cat++ <69035887+catplvsplus@users.noreply.github.com>
2025-12-29 22:49:17 +0800

    add pwa

--- apps/website/vite.config.ts
+++ apps/website/vite.config.ts
@@ -1,5 +1,12 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite';
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
+export default defineConfig({
+    plugins: [
+        tailwindcss(),
+        sveltekit(),
+        VitePWA({ registerType: 'autoUpdate' })
+    ]
+});
