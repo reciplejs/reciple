7969cec962387fc56f3361db772db32174ea7816
Cat++ <69035887+catplvsplus@users.noreply.github.com>
2026-01-02 22:33:00 +0800

    remove mockup components

--- apps/website/src/lib/components/shared/main/Sidebar.svelte
+++ apps/website/src/lib/components/shared/main/Sidebar.svelte
@@ -1,11 +1,7 @@
 <script lang="ts">
     import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarMenuSub, SidebarMenuSubButton, SidebarMenuSubItem, SidebarSeparator } from "$lib/components/ui/sidebar";
-    import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '$lib/components/ui/dropdown-menu';
     import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '$lib/components/ui/collapsible';
-    import ChevronsUpDownIcon from '@lucide/svelte/icons/chevrons-up-down';
     import ChevronRightIcon from '@lucide/svelte/icons/chevron-right';
-    import PackageIcon from '@lucide/svelte/icons/package';
-    import GalleryHorizontalEndIcon from '@lucide/svelte/icons/gallery-horizontal-end';
     import SparklesIcon from '@lucide/svelte/icons/sparkles';
     import SearchIcon from '@lucide/svelte/icons/search';
     import type { WithElementRef } from '$lib/helpers/utils';
@@ -13,6 +9,8 @@
     import type { Snippet } from 'svelte';
     import { sidebarData } from '../../../helpers/contexts';
     import { page } from '$app/state';
+    import { resolve } from '$app/paths';
+    import { goto } from '$app/navigation';
 
     let {
         header,
@@ -35,7 +33,7 @@
     <SidebarHeader>
         <SidebarMenu>
             <SidebarMenuItem>
-                <SidebarMenuButton size="lg" class="bg-transparent!">
+                <SidebarMenuButton onclick={() => goto(resolve('/(home)'))} size="lg" class="cursor-pointer">
                     <div class="bg-primary text-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
                         <SparklesIcon class="size-5"/>
                     </div>
@@ -47,7 +45,7 @@
         </SidebarMenu>
         <SidebarSeparator/>
         <SidebarMenu>
-            <SidebarMenuItem class="mb-2">
+            <SidebarMenuItem>
                 <SidebarMenuButton
                     tooltipContent="Search"
                     class="w-full rounded-md justify-start overflow-hidden bg-sidebar-accent/50 border border-sidebar-border"
@@ -55,90 +53,6 @@
                     <SearchIcon/>
                     <span>Search</span>
                 </SidebarMenuButton>
-            </SidebarMenuItem>
-            <SidebarMenuItem>
-                <DropdownMenu>
-                    <DropdownMenuTrigger>
-                        {#snippet child({ props })}
-                            <SidebarMenuButton
-                                {...props}
-                                size="lg"
-                                tooltipContent="Packages"
-                                class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-                            >
-                                <div class="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-                                    <PackageIcon class="size-4.5"/>
-                                </div>
-                                <div class="grid flex-1 text-start text-sm leading-tight">
-                                    <span class="truncate font-medium">
-                                        reciple
-                                    </span>
-                                </div>
-                                <ChevronsUpDownIcon class="ms-auto" />
-                            </SidebarMenuButton>
-                        {/snippet}
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent class="w-(--bits-dropdown-menu-anchor-width) text-nowrap min-w-2xs mx-2">
-                        <DropdownMenuItem>
-                            <PackageIcon/>
-                            <span class="truncate">@reciple/core</span>
-                        </DropdownMenuItem>
-                        <DropdownMenuItem>
-                            <PackageIcon/>
-                            <span class="truncate">@reciple/utils</span>
-                        </DropdownMenuItem>
-                        <DropdownMenuItem>
-                            <PackageIcon/>
-                            <span class="truncate">@reciple/jsx</span>
-                        </DropdownMenuItem>
-                        <DropdownMenuItem>
-                            <PackageIcon/>
-                            <span class="truncate">@reciple/modules</span>
-                        </DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-            </SidebarMenuItem>
-            <SidebarMenuItem>
-                <DropdownMenu>
-                    <DropdownMenuTrigger>
-                        {#snippet child({ props })}
-                            <SidebarMenuButton
-                                {...props}
-                                size="lg"
-                                tooltipContent="Versions"
-                                class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-                            >
-                                <div class="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-                                    <GalleryHorizontalEndIcon class="size-4.5"/>
-                                </div>
-                                <div class="grid flex-1 text-start text-sm leading-tight">
-                                    <span class="truncate font-medium">
-                                        10.0.1
-                                    </span>
-                                </div>
-                                <ChevronsUpDownIcon class="ms-auto" />
-                            </SidebarMenuButton>
-                        {/snippet}
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent class="w-(--bits-dropdown-menu-anchor-width) text-nowrap min-w-2xs mx-2">
-                        <DropdownMenuItem>
-                            <PackageIcon/>
-                            <span class="truncate">main</span>
-                        </DropdownMenuItem>
-                        <DropdownMenuItem>
-                            <PackageIcon/>
-                            <span class="truncate">9.0.0</span>
-                        </DropdownMenuItem>
-                        <DropdownMenuItem>
-                            <PackageIcon/>
-                            <span class="truncate">8.0.0</span>
-                        </DropdownMenuItem>
-                        <DropdownMenuItem>
-                            <PackageIcon/>
-                            <span class="truncate">7.0.0</span>
-                        </DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
             </SidebarMenuItem>
         </SidebarMenu>
         {@render header?.()}
---
+++ apps/website/src/lib/components/shared/main/SidebarPackageInfo.svelte
@@ -0,0 +1,92 @@
+<script lang="ts">
+    import { SidebarMenuButton, SidebarMenuItem } from "$lib/components/ui/sidebar";
+    import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '$lib/components/ui/dropdown-menu';
+    import ChevronsUpDownIcon from '@lucide/svelte/icons/chevrons-up-down';
+    import PackageIcon from '@lucide/svelte/icons/package';
+    import GalleryHorizontalEndIcon from '@lucide/svelte/icons/gallery-horizontal-end';
+</script>
+
+<SidebarMenuItem>
+    <DropdownMenu>
+        <DropdownMenuTrigger>
+            {#snippet child({ props })}
+                <SidebarMenuButton
+                    {...props}
+                    size="lg"
+                    tooltipContent="Packages"
+                    class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                >
+                    <div class="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+                        <PackageIcon class="size-4.5"/>
+                    </div>
+                    <div class="grid flex-1 text-start text-sm leading-tight">
+                        <span class="truncate font-medium">
+                            reciple
+                        </span>
+                    </div>
+                    <ChevronsUpDownIcon class="ms-auto" />
+                </SidebarMenuButton>
+            {/snippet}
+        </DropdownMenuTrigger>
+        <DropdownMenuContent class="w-(--bits-dropdown-menu-anchor-width) text-nowrap min-w-2xs mx-2">
+            <DropdownMenuItem>
+                <PackageIcon/>
+                <span class="truncate">@reciple/core</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+                <PackageIcon/>
+                <span class="truncate">@reciple/utils</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+                <PackageIcon/>
+                <span class="truncate">@reciple/jsx</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+                <PackageIcon/>
+                <span class="truncate">@reciple/modules</span>
+            </DropdownMenuItem>
+        </DropdownMenuContent>
+    </DropdownMenu>
+</SidebarMenuItem>
+<SidebarMenuItem>
+    <DropdownMenu>
+        <DropdownMenuTrigger>
+            {#snippet child({ props })}
+                <SidebarMenuButton
+                    {...props}
+                    size="lg"
+                    tooltipContent="Versions"
+                    class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                >
+                    <div class="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+                        <GalleryHorizontalEndIcon class="size-4.5"/>
+                    </div>
+                    <div class="grid flex-1 text-start text-sm leading-tight">
+                        <span class="truncate font-medium">
+                            10.0.1
+                        </span>
+                    </div>
+                    <ChevronsUpDownIcon class="ms-auto" />
+                </SidebarMenuButton>
+            {/snippet}
+        </DropdownMenuTrigger>
+        <DropdownMenuContent class="w-(--bits-dropdown-menu-anchor-width) text-nowrap min-w-2xs mx-2">
+            <DropdownMenuItem>
+                <GalleryHorizontalEndIcon/>
+                <span class="truncate">main</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+                <GalleryHorizontalEndIcon/>
+                <span class="truncate">9.0.0</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+                <GalleryHorizontalEndIcon/>
+                <span class="truncate">8.0.0</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+                <GalleryHorizontalEndIcon/>
+                <span class="truncate">7.0.0</span>
+            </DropdownMenuItem>
+        </DropdownMenuContent>
+    </DropdownMenu>
+</SidebarMenuItem>
