b4cd82834278b71b3a59c3adec0a764f3bf25dc3
Cat++ <69035887+catplvsplus@users.noreply.github.com>
2026-01-03 16:05:18 +0800

    remove quote

--- apps/website/src/routes/(main)/guide/[...slug]/+page.svelte
+++ apps/website/src/routes/(main)/guide/[...slug]/+page.svelte
@@ -69,6 +69,7 @@
         "prose prose-neutral prose-sm md:prose-base dark:prose-invert max-w-none p-4",
         "prose-code:after:content-none prose-code:before:content-none prose-code:bg-foreground/15 prose-code:py-0.5 prose-code:px-1 prose-code:rounded-md",
         "prose-pre:prose-code:rounded-none prose-pre:prose-code:p-0 prose-pre:prose-code:bg-transparent prose-pre:leading-tight",
+        "prose-blockquote:prose-p:before:content-none prose-blockquote:prose-p:after:content-none",
         "prose-a:text-primary dark:prose-a:text-blue-400 prose-a:no-underline"
     ]}
 >
