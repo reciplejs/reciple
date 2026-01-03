0e9ddd5b761cd1cace7ea4ae8d19f253c1f876c9
Cat++ <69035887+catplvsplus@users.noreply.github.com>
2026-01-03 17:39:26 +0800

    subtle blur

--- apps/website/src/lib/components/shared/main/Header.svelte
+++ apps/website/src/lib/components/shared/main/Header.svelte
@@ -13,8 +13,9 @@
 
 <header
     class={[
-        "sticky top-0 z-50 bg-background flex h-16 border-b shrink-0 items-center gap-2",
-        "transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12"
+        "sticky top-0 z-50 flex h-16 border-b shrink-0 items-center gap-2",
+        "transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12",
+        "bg-background/80 backdrop-blur-md"
     ]}
 >
     <div class="flex items-center gap-2 px-4 overflow-hidden w-full">
