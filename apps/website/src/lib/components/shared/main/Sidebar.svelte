0cf7d3a95bfc687beda6d5961fa6006a8bb0939f
Cat++ <69035887+catplvsplus@users.noreply.github.com>
2026-01-18 12:29:08 +0800

    separate sidebar components

--- apps/website/src/lib/components/shared/main/Sidebar.svelte
+++ apps/website/src/lib/components/shared/main/Sidebar.svelte
@@ -1,18 +1,15 @@
 <script lang="ts">
-    import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarMenuSub, SidebarMenuSubButton, SidebarMenuSubItem, SidebarSeparator, useSidebar } from "$lib/components/ui/sidebar";
-    import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '$lib/components/ui/collapsible';
-    import ChevronRightIcon from '@lucide/svelte/icons/chevron-right';
+    import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarSeparator } from "$lib/components/ui/sidebar";
     import SparklesIcon from '@lucide/svelte/icons/sparkles';
     import SearchIcon from '@lucide/svelte/icons/search';
     import type { WithElementRef } from '$lib/helpers/utils';
     import type { HTMLAttributes } from 'svelte/elements';
     import type { Snippet } from 'svelte';
     import { searchDialogState, sidebarData } from '$lib/helpers/contexts';
-    import { page } from '$app/state';
     import { resolve } from '$app/paths';
     import { goto } from '$app/navigation';
-    import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '$lib/components/ui/dropdown-menu';
-    import { ChevronsUpDownIcon } from '@lucide/svelte';
+    import SidebarMenus from './sidebar/SidebarMenus.svelte';
+    import SidebarGroups from './sidebar/SidebarGroups.svelte';
 
     let {
         header,
@@ -64,118 +61,13 @@
             </SidebarMenu>
         {/if}
         {#if data.header?.menus?.length}
-            {@const sidebar = useSidebar()}
-            <SidebarMenu>
-                {#each data.header.menus as menu}
-                    {@const active = menu.active ? menu.items.find(i => i.name === menu.active) : null}
-                    <SidebarMenuItem>
-                        <DropdownMenu>
-                            <DropdownMenuTrigger>
-                                {#snippet child({ props })}
-                                    <SidebarMenuButton
-                                        {...props}
-                                        size="lg"
-                                        tooltipContent={menu.label}
-                                    >
-                                        <div class="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-                                            {#if active?.icon}
-                                                <active.icon class="size-4.5"/>
-                                            {:else if menu.icon}
-                                                <menu.icon class="size-4.5"/>
-                                            {/if}
-                                        </div>
-                                        <div class="grid flex-1 text-start text-sm leading-tight">
-                                            <span class="truncate font-medium">{active?.name || menu.label}</span>
-                                        </div>
-                                        <ChevronsUpDownIcon class="ms-auto" />
-                                    </SidebarMenuButton>
-                                {/snippet}
-                            </DropdownMenuTrigger>
-                            <DropdownMenuContent
-                                class="w-(--bits-dropdown-menu-anchor-width) text-nowrap min-w-2xs mx-2"
-                                side={sidebar.open ? "bottom" : "right"}
-                                align={sidebar.open ? "center" : "start"}
-                            >
-                                {#each menu.items as item}
-                                    {@const isActive = item.name === active?.name}
-                                    <DropdownMenuItem
-                                        class={[
-                                            isActive && "text-primary! font-medium bg-primary/10! dark:bg-primary! dark:text-primary-foreground!",
-                                        ]}
-                                        onclick={() => item.external ? window.open(item.href, '_blank') : goto(item.href)}
-                                    >
-                                        {#if item.icon}
-                                            <item.icon/>
-                                        {/if}
-                                        <span>{item.name}</span>
-                                    </DropdownMenuItem>
-                                {/each}
-                            </DropdownMenuContent>
-                        </DropdownMenu>
-                    </SidebarMenuItem>
-                {/each}
-            </SidebarMenu>
+            <SidebarMenus data={data.header.menus}/>
         {/if}
         {@render header?.()}
     </SidebarHeader>
     <SidebarContent>
         {#if data.content?.groups}
-            {#each data.content.groups as group (group.label)}
-                <SidebarGroup>
-                    {#if group.label}
-                        <SidebarGroupLabel>
-                            {group.label}
-                        </SidebarGroupLabel>
-                    {/if}
-                    <SidebarGroupContent>
-                        {#each Object.keys(group.categories) as label (label)}
-                            {@const category = group.categories[label]}
-                            {#if category.links.length}
-                                <SidebarMenu>
-                                    <Collapsible open class="group/collapsible overflow-hidden">
-                                        {#snippet child({ props })}
-                                            <SidebarMenuItem {...props}>
-                                                <CollapsibleTrigger class="z-10 relative">
-                                                    {#snippet child({ props })}
-                                                        <SidebarMenuButton {...props}>
-                                                            {#if category.icon}
-                                                                <category.icon/>
-                                                            {/if}
-                                                            <span>{label}</span>
-                                                            <ChevronRightIcon class="ms-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90"/>
-                                                        </SidebarMenuButton>
-                                                    {/snippet}
-                                                </CollapsibleTrigger>
-                                                <CollapsibleContent class="data-[state=closed]:animate-out data-[state=open]:animate-in data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0">
-                                                    <SidebarMenuSub>
-                                                        {#each category.links as item (item.label + item.href)}
-                                                            {@const isActive = !!page.params.slug && item.href.endsWith(page.params.slug)}
-                                                            <SidebarMenuSubItem>
-                                                                <SidebarMenuSubButton
-                                                                    href={item.href}
-                                                                    target={item.external ? '_blank' : undefined}
-                                                                    class={[
-                                                                        isActive && "text-primary! font-medium bg-primary/10! dark:bg-primary! dark:text-primary-foreground!",
-                                                                    ]}
-                                                                >
-                                                                    {#if item.icon}
-                                                                        <item.icon/>
-                                                                    {/if}
-                                                                    <span>{item.label}</span>
-                                                                </SidebarMenuSubButton>
-                                                            </SidebarMenuSubItem>
-                                                        {/each}
-                                                    </SidebarMenuSub>
-                                                </CollapsibleContent>
-                                            </SidebarMenuItem>
-                                        {/snippet}
-                                    </Collapsible>
-                                </SidebarMenu>
-                            {/if}
-                        {/each}
-                    </SidebarGroupContent>
-                </SidebarGroup>
-            {/each}
+            <SidebarGroups data={data.content.groups}/>
         {/if}
         {@render content?.()}
     </SidebarContent>
