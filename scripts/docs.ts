daed3ff34f86e4057b5cecec83aea3b194c4be68
Cat++ <69035887+catplvsplus@users.noreply.github.com>
2026-01-23 19:38:57 +0800

    replace with link

--- scripts/docs.ts
+++ scripts/docs.ts
@@ -1,13 +1,14 @@
-import { spawn } from "node:child_process";
+import { execSync, spawn } from "node:child_process";
 import { mkdir, readdir, stat, rm, readFile, writeFile } from "node:fs/promises";
 import path from "node:path";
 
 const root = path.join(import.meta.dirname, '../');
 const packages = await readdir(path.join(root, "./packages"));
 const outDir = path.join(root, './docs');
+const gitPath = `${await getGitRemote()}/blob/${await getGitRef()}/`;
 
 await new Promise((res, rej) => {
-    const child = spawn(`turbo run docs`, {
+    const child = spawn(`turbo run docs --cache=local:`, {
         env: process.env,
         shell: true,
         stdio: 'inherit'
@@ -30,7 +31,7 @@ for (const pkg of packages) {
     const exists = await stat(entry).then(r => r.isFile()).catch(() => false);
     if (!exists) continue;
 
-    const data = JSON.parse(await readFile(entry, 'utf-8').then(data => data.replaceAll(`file://${path.join(root, 'packages/')}`, './')));
+    const data = JSON.parse(await readFile(entry, 'utf-8').then(data => data.replaceAll(`file://${path.join(root)}`, gitPath)));
 
     const readme = path.join("./packages", pkg, "README.md");
 
@@ -41,4 +42,16 @@ for (const pkg of packages) {
     await rm(entry);
     await mkdir(newDir, { recursive: true });
     await writeFile(path.join(newDir, `docs.json`), JSON.stringify(data, null, 2));
+}
+
+async function getGitRef() {
+    const child = execSync(`git rev-parse HEAD`);
+    return child.toString().trim();
+}
+
+async function getGitRemote() {
+    const child = execSync(`git config --get remote.origin.url`);
+    const remote = child.toString().trim();
+
+    return remote.endsWith('.git') ? remote.slice(0, -4) : remote;
 }
