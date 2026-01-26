3cee6dcb71458347e082e53f45218641488ed211
Cat++ <69035887+catplvsplus@users.noreply.github.com>
2026-01-26 18:13:10 +0800

    lol

--- apps/website/vite.config.ts
+++ apps/website/vite.config.ts
@@ -1,39 +1,10 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [
         tailwindcss(),
-        sveltekit(),
-        VitePWA({
-            registerType: 'autoUpdate',
-            manifest: {
-                icons: [
-                    {
-                        src: "pwa-64x64.png",
-                        sizes: "64x64",
-                        type: "image/png"
-                    },
-                    {
-                        src: "pwa-192x192.png",
-                        sizes: "192x192",
-                        type: "image/png"
-                    },
-                    {
-                        src: "pwa-512x512.png",
-                        sizes: "512x512",
-                        type: "image/png"
-                    },
-                    {
-                        src: "maskable-icon-512x512.png",
-                        sizes: "512x512",
-                        type: "image/png",
-                        purpose: "maskable"
-                    }
-                ]
-            },
-        })
+        sveltekit()
     ]
 });
