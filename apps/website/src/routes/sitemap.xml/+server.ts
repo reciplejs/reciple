9d23715d541de13d81572dbb990b26ed83989664
Cat++ <69035887+catplvsplus@users.noreply.github.com>
2026-01-27 19:22:12 +0800

    sitemap

---
+++ apps/website/src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,75 @@
+import { resolve } from '$app/paths';
+import { Documentation } from '../../lib/helpers/classes/Documentation.svelte';
+import type { MarkdownModules } from '../../lib/helpers/types';
+import { normalizeGuideCategoryId, normalizeGuidePageId } from '../../lib/helpers/utils';
+import path from 'node:path';
+
+export async function GET({ fetch, url }) {
+    const entries: string[] = [];
+    const packages = await Documentation.fetchPackages(fetch);
+
+    entries.push(createSitemapEntry(url.origin, { priority: '1.0' }));
+    entries.push(createSitemapEntry(resolve('/(home)'), { priority: '0.80' }));
+
+    for (const pkg of packages) {
+        const tags = await Documentation.fetchTags(pkg, fetch);
+        if (!tags.length) continue;
+
+        entries.push(createSitemapEntry(path.join(
+            url.origin,
+            resolve('/docs/[package]', { package: pkg })), { priority: '0.50' })
+        );
+
+        for (const tag of tags) {
+            entries.push(createSitemapEntry(path.join(
+                url.origin,
+                resolve('/(main)/docs/[package]/[tag]', { package: pkg, tag })
+            ), { priority: '0.40' }));
+        }
+
+        const firstTag = tags[0];
+        const documentation = await (new Documentation({ package: pkg, tag: firstTag })).fetch(fetch);
+
+        for (const node of documentation.data) {
+            entries.push(createSitemapEntry(path.join(
+                url.origin,
+                resolve('/(main)/docs/[package]/[tag]/[...slug]', { package: pkg, tag: firstTag, slug: node.name })
+            ), { priority: '0.30' }));
+        }
+    }
+
+    const guides = import.meta.glob(`$lib/guide/*/*{.md,.svx}`) as MarkdownModules;
+
+    for (const [loc, module] of Object.entries(guides)) {
+        const { metadata } = await module();
+        if (!metadata) continue;
+
+        const parts = loc.split('/');
+        const pageId = normalizeGuidePageId(parts.pop() ?? '');
+        const categoryId = normalizeGuideCategoryId(parts.pop() ?? '');
+
+        if (!pageId || !categoryId) continue;
+
+        entries.push(createSitemapEntry(path.join(
+            url.origin,
+            resolve('/(main)/guide/[...slug]', { slug: `${categoryId}/${pageId}` })
+        )));
+    }
+
+    const xml = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">${entries.join('')}</urlset>`;
+
+    return new Response(xml, {
+        headers: {
+            'Content-Type': 'application/xml',
+            'Content-Length': xml.length.toString(),
+            'Cache-Control': 'max-age=3600',
+            'Access-Control-Allow-Origin': '*'
+        }
+    })
+}
+
+function createSitemapEntry(url: string, data?: Record<string, string>): string {
+    const loc = `<loc>${url}</loc>`;
+    const others = Object.entries(data || {}).map(([key, value]) => `<${key}>${value}</${key}>`).join('');
+    return `<url>${loc}${others}</url>`;
+}
