afdb3e911fb0f516050d3d747dce54b35bd73198
Cat++ <69035887+catplvsplus@users.noreply.github.com>
2026-01-31 12:50:51 +0800

    eliminate declaring variable that is only used once

--- example/src/commands/v2.tsx
+++ example/src/commands/v2.tsx
@@ -8,19 +8,15 @@ export class V2 extends SlashCommandModule {
         .toJSON();
 
     public async execute(data: SlashCommand.ExecuteData): Promise<void> {
-        const components = <>
-            <TextDisplay># V2 Components</TextDisplay>
-            <Section>
-                <Thumbnail url='https://avatars.githubusercontent.com/u/69035887?v=4'/>
-                <TextDisplay>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident repellat fuga officiis minus enim, consequatur blanditiis suscipit, itaque ducimus voluptatibus tempora est recusandae eos reprehenderit ipsa rerum, laudantium nam voluptatem.</TextDisplay>
-            </Section>
-        </>;
-
-        useLogger().log(components);
-
         await data.interaction.reply({
             flags: ['IsComponentsV2'],
-            components
+            components: <>
+                <TextDisplay># V2 Components</TextDisplay>
+                <Section>
+                    <Thumbnail url='https://avatars.githubusercontent.com/u/69035887?v=4'/>
+                    <TextDisplay>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident repellat fuga officiis minus enim, consequatur blanditiis suscipit, itaque ducimus voluptatibus tempora est recusandae eos reprehenderit ipsa rerum, laudantium nam voluptatem.</TextDisplay>
+                </Section>
+            </>
         })
     }
 }
