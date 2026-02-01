6346abdd19e743f6df58554dbc9c3cb4302157f0
Cat++ <69035887+catplvsplus@users.noreply.github.com>
2026-02-01 12:37:00 +0800

    filter undefined children that causes error on some components

--- packages/jsx/src/structures/JSX.ts
+++ packages/jsx/src/structures/JSX.ts
@@ -15,6 +15,6 @@ export namespace JSX {
     }
 
     export function useSingleToArray<T = any>(value: T|T[]): T[] {
-        return Array.isArray(value) ? value.flat() as T[] : [value];
+        return (Array.isArray(value) ? value.flat() : [value]).filter(t => typeof t !== 'undefined') as T[];
     }
 }
