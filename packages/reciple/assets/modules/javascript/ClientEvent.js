6e57ead19b06a38f3b10f1163857e0b3ccf3b648
Cat++ <69035887+catplvsplus@users.noreply.github.com>
2026-02-21 21:12:42 +0800

    fix: add missing ts-expect-error comments for type checking

--- packages/reciple/assets/modules/javascript/ClientEvent.js
+++ packages/reciple/assets/modules/javascript/ClientEvent.js
@@ -1,11 +1,13 @@
 // @ts-check
 import { ClientEventModule } from "reciple";
 
+// @ts-expect-error
 export class $MODULE_NAME$ extends ClientEventModule {
     event = '$EVENT_NAME$';
     // @ts-expect-error
     once = $EVENT_ONCE$;
 
+    // @ts-expect-error
     onEvent(...args) {
         // Write your code here
     }
